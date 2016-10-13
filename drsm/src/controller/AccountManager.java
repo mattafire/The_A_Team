a87c1169fe3775969c6581ee07377627c02f2395
Bleak-Dalas <dbleak42@gmail.com>
2016-10-12 22:31:18 -0600

    Created AccountManager Class and added default methods and variables

---
+++ drsm/src/controller/AccountManager.java
@@ -0,0 +1,37 @@
+
+package controller;
+
+import java.util.ArrayList;
+
+
+
+
+public class AccountManager {
+    
+    private ArrayList accounts;
+    
+    
+    // class methods
+    public void createAccount(){
+        
+    }
+    
+    public void updateAccount() {
+        
+    }
+    
+    public void deleteAccount() {
+        
+    }
+    
+    // getters
+    public ArrayList getAccounts() {
+        return accounts;
+    }
+
+    // setters
+    public void setAccounts(ArrayList accounts) {
+        this.accounts = accounts;
+    }
+    
+}
