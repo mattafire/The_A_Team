c68440fdf6bc2ebb4cd571fb0fdcaa236ac7898e
Bleak-Dalas <dbleak42@gmail.com>
2016-10-29 16:27:38 -0600

    account manager modified

--- drsm/src/controller/AccountManager.java
+++ drsm/src/controller/AccountManager.java
@@ -2,6 +2,13 @@
 package controller;
 
 import java.util.ArrayList;
+import model.BudgetAccount;
+import model.Deposits;
+import model.Expenses;
+import model.HibernateInterface;
+import view.AddDepositView;
+import view.CreateAccountView;
+import view.UpdateAccountView;
 
 
 
@@ -9,15 +16,28 @@ import java.util.ArrayList;
 public class AccountManager {
     
     private ArrayList accounts;
+    BudgetAccount bAccounts = new BudgetAccount();
+    Deposits deposit = new Deposits();
+    Expenses expense =  new Expenses();
+    CreateAccountView createAccount = new CreateAccountView();
+    UpdateAccountView updateAccount = new UpdateAccountView();
+    AddDepositView addDeposit = new AddDepositView();
+    HibernateInterface hib = new HibernateInterface();
+
     
     
     // class methods
     public void createAccount(){
-        
+        createAccount.createAccount(bAccounts);
+        hib.updateAccount(bAccounts);
     }
     
-    public void updateAccount() {
-        
+    public void updateAccounts() {
+        updateAccount.updateAccount(updateAccount);
+    }
+
+    public void addDeposit() {
+        addDeposit.addDeposit(bAccounts, deposit);
     }
     
     public void deleteAccount() {
